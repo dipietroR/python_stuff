2a2f19331ab4257864efe7de53918118eb6d8e08
Raquel <raquel@awesome>
2020-06-10 16:52:33 -0400

    rock_paper_scissor_scoring

--- rock_paper_scissor/play_now.py
+++ rock_paper_scissor/play_now.py
@@ -1,31 +1,46 @@
 import random
 
-computer_choice=random.randint(1,3)
-#print(computer_choice) #debug 
+user_score = 0
+computer_score = 0
 
-print ("1==Rock")
-print ("2==Scissor")
-print ("3==Paper")
+while user_score < 5 and computer_score < 5:
+    
+    computer_choice=random.randint(1,3)
+    #print(computer_choice) #debug 
 
-user_choice = int(input("enter a number between 1,3: "))
+    print ("1==Rock")
+    print ("2==Scissor")
+    print ("3==Paper")
 
-if (user_choice==computer_choice):
-    print("Tie!")
-elif(user_choice==1 and computer_choice==2):
-    print("user chose ROCK, computer chose SCISSOR")
-    print("user wins!") 
-elif(user_choice==1 and computer_choice==3):
-    print("user chose ROCK, computer chose PAPER")
-    print("computer wins!") 
-elif(user_choice==2 and computer_choice==1):
-    print("user chose SCISSOR, computer chose ROCK")
-    print("computer wins!") 
-elif(user_choice==2 and computer_choice==3):
-    print("user chose SCISSOR, computer chose PAPER")
-    print("user wins!") 
-elif(user_choice==3 and computer_choice==1):
-    print("user chose PAPER, computer chose ROCK")
-    print("user wins!") 
-elif(user_choice==3 and computer_choice==2):
-    print("user chose PAPER, computer chose SCISSOR")
-    print("computer wins!") 
+    user_choice = int(input("enter a number between 1,3: "))
+
+    if (user_choice==computer_choice):
+        print("Tie!")
+    elif(user_choice==1 and computer_choice==2):
+        print("user chose ROCK, computer chose SCISSOR")
+        print("user wins!") 
+        user_score = user_score + 1
+    elif(user_choice==1 and computer_choice==3):
+        print("user chose ROCK, computer chose PAPER")
+        print("computer wins!") 
+        computer_score = computer_score + 1
+    elif(user_choice==2 and computer_choice==1):
+        print("user chose SCISSOR, computer chose ROCK")
+        print("computer wins!") 
+        computer_score = computer_score + 1
+    elif(user_choice==2 and computer_choice==3):
+        print("user chose SCISSOR, computer chose PAPER")
+        print("user wins!") 
+        user_score = user_score + 1
+    elif(user_choice==3 and computer_choice==1):
+        print("user chose PAPER, computer chose ROCK")
+        print("user wins!") 
+        user_score = user_score + 1
+    elif(user_choice==3 and computer_choice==2):
+        print("user chose PAPER, computer chose SCISSOR")
+        print("computer wins!") 
+        computer_score = computer_score + 1
+
+    print("user: " + str(user_score) + " and computer: " + str(computer_score))
+
+
